268658c69573dbf4141ac0c65d15cba500378321
Vilem-McKael <vilemckael@gmail.com>
2023-03-27 14:56:57 -0700

    adds delete post functionality

--- can-i-share-my-screen-express/controllers/posts.js
+++ can-i-share-my-screen-express/controllers/posts.js
@@ -6,7 +6,7 @@ const Post = require('../models/post');
 
 async function showAll (req, res) {
   const posts = await Post.find({});
-  await console.log(`HERE ARE OUR POSTS ${posts}`);
+  // await console.log(`HERE ARE OUR POSTS ${posts}`);
   res.render('posts/all', { title: 'All Posts', posts });
 }
 
@@ -32,20 +32,44 @@ function showMy (req, res) {
 }
 
 async function showOne (req, res) {
-const post = await Post.findById(req.params.id).populate('comments')
-console.log(post.cast)
-//const performers = await Pe.find({_id: {$nin: movie.cast}})
-// console.log(performers)
-res.render('posts/show', { 
-  title: `${post.title}`,
-  post
-});
+  const post = await Post.findById(req.params.id).populate('comments')
+  //const performers = await Pe.find({_id: {$nin: movie.cast}})
+  // console.log(performers)
+  res.render('posts/show', { 
+    title: `${post.title}`,
+    post
+  });
+}
+
+async function deleteOnePost(req, res) {
+  try {
+    const post = await Post.findById(req.params.id);
+    await Post.findOneAndDelete({_id: post._id});
+    console.log(res);
+    // ***************** ASK KEN WHY ITS WORKING WITHOUT A REDIRECT
+  } catch(err) {
+    console.error(err);
+    res.redirect('/posts/all');
+  }
+
+
+  Post.findById(req.params.id).then(function(post) {
+    if (!post) return res.redirect('/posts/all');
+    post.comments.remove(req.body.commentId);
+    post.save().then(function() {
+      res.redirect(`/posts/${post._id}`);
+    }).catch(function(err) {
+      // Let Express display an error
+      console.log(err);
+      return next(err);
+    });
+  })
 }
 
 module.exports = {
   showAll,
   newPost,
   createPost,
-  showOne
-
+  showOne,
+  deleteOnePost
 }
--- can-i-share-my-screen-express/routes/posts.js
+++ can-i-share-my-screen-express/routes/posts.js
@@ -8,5 +8,6 @@ router.get('/all', postsCtrl.showAll);
 router.get('/new', postsCtrl.newPost);
 router.post('/all', postsCtrl.createPost);
 router.get('/:id', postsCtrl.showOne);
+router.delete('/:id', postsCtrl.deleteOnePost);
 
 module.exports = router;
